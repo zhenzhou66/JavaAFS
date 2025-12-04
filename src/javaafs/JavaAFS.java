700fa65b823b4a23bb5e87bedfef95a0f80174ce
junjun <junjun@junjun>
2025-12-05 02:52:06 +0800

    hihihi

--- src/javaafs/JavaAFS.java
+++ src/javaafs/JavaAFS.java
@@ -16,6 +16,7 @@ public class JavaAFS {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello im zhenzhou");
+        System.out.println("hello im lijun");
     }
     
 }
