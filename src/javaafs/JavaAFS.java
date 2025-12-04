ef6777685c4e0735bdfc8a78e179d370a613b9a4
zhenzhou66 <zhenzhou0609@gmail.com>
2025-12-05 02:14:28 +0800

    update 2

--- src/javaafs/JavaAFS.java
+++ src/javaafs/JavaAFS.java
@@ -15,7 +15,7 @@ public class JavaAFS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world");
+        System.out.println("hello update 2");
     }
     
 }
