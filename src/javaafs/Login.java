2e64526134773178506cfbe75f9cdb1f0fb26b09
zhenzhou66 <zhenzhou0609@gmail.com>
2026-02-05 10:39:37 +0800

    Lecturer Main Menu update

--- src/javaafs/Login.java
+++ src/javaafs/Login.java
@@ -148,7 +148,7 @@ public class Login extends javax.swing.JFrame {
 
             } else if (userType.equals("Lecturer")) {
                 Status.setText("Access granted!");
-                LecturerMainMenu menu = new LecturerMainMenu();
+                LecturerMainMenu menu = new LecturerMainMenu(username);
                 this.setVisible(false);
                 menu.setVisible(true);
 
