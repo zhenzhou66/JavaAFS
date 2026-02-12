2825b05f51151cb8310fbb45fcff97892cb14f73
junjun <junjun@junjun>
2026-02-12 21:59:41 +0800

    pull from main

--- src/javaafs/StudentMainMenu.java
+++ src/javaafs/StudentMainMenu.java
@@ -72,14 +72,20 @@ public class StudentMainMenu extends javax.swing.JFrame {
         if (userArray == null || userArray.isEmpty()) 
             return;
 
-        String userModuleID = "";
-
-        for (int i = 0; i < userArray.size(); i++) {
-            String[] user = userArray.get(i);
-            if (user[0].equalsIgnoreCase(userid)) {
-                lecturerName.setText(user[3]);
-                userRole.setText(user[2]); 
-                userModuleID = user[6];
+    for (int i = 0; i < userArray.size(); i++) {
+        String[] user = userArray.get(i);
+        if (user[0].equalsIgnoreCase(userid)) {
+            lecturerName.setText(user[3]);
+            userRole.setText(user[2]); 
+            userModuleID = user[6];
+            break;
+        }
+    }
+    if (!userModuleID.isEmpty()) {
+        for (String[] module : moduleArray) {
+            if (module[0].equalsIgnoreCase(userModuleID)) {
+                ModuleName = module[1]; // Index 1 is moduleName in modules.txt
+                courseName.setText(ModuleName); 
                 break;
             }
         }
